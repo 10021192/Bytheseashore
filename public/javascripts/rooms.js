a7c8ecf69c14731a6a6b1039a3ebc6abd72b8143
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2022-04-23 16:02:11 +0100

    only index.html exists now

--- public/javascripts/rooms.js
+++ public/javascripts/rooms.js
@@ -1,4 +1,4 @@
-window.onload = async function() {
+/*window.onload = async function() {
     let userInfo = await getUserInfo();
     document.getElementById("username").innerHTML = userInfo.ply_name;   
-}
+}*/
